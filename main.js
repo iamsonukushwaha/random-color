091caea9ab32438dd4e757eb92971e1c83f5d508
Sonu kumar kushwaha <51280276+flyingsonu122@users.noreply.github.com>
2020-06-26 14:55:27 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+//variables
+let color = "#";
+const colors = [
+    0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'
+]
+const btn = document.getElementById('btn');
+const bodyBackground = document.getElementById('body');
+const value = document.querySelector('.value');
+//logic
+
+btn.addEventListener('click',getColor);
+
+function getColor() {
+    color = "#";
+    for(let i = 0; i < 6; i++){
+        let random = Math.floor(Math.random() * colors.length);
+
+        color += colors[random];
+    }
+    bodyBackground.style.backgroundColor = color;
+    value.innerHTML = color;
+}
+
